54db1b6e30b99bd9a0681960712007feb5401aa2
Zihan Kuang <Zihan_kuang@outlook.com>
2025-10-11 00:02:22 +0000

    add async back
    Signed-off-by: Zihan Kuang <Zihan_kuang@outlook.com>

--- homeassistant/components/buienradar/util.py
+++ homeassistant/components/buienradar/util.py
@@ -70,7 +70,7 @@ class BrData:
         self.timeframe = timeframe
         self.unsub_schedule_update: CALLBACK_TYPE | None = None
 
-    def update_devices(self):
+    async def update_devices(self):
         """Update all devices/sensors."""
         if not self.devices:
             return
